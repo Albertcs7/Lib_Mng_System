56649688763cf2d13b447af95f758d4864824c45
Albert C S <albertacs@gmail.com>
2026-01-30 15:32:50 +0530

    second commit

--- mng_app/views.py
+++ mng_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 
 def home(request):
     return render(request,"home.html")
@@ -7,11 +7,9 @@ def add(request):
     if request.method == 'POST':
         book_name = request.POST.get('bname')
         book_auth = request.POST.get('aname')
-        shelf = request.POST.get('shelf')
+        shelf = request.POST.get('sno')
         book_cat = request.POST.get('cat')
-        
-        a = [book_name, book_auth, shelf, book_cat]
-
+        a = { "name":book_name, "auth":book_auth, "shel":shelf, "cat":book_cat }
         return render(request, "view.html", {"details": a})
     return render(request, "add.html")
 
